fa81e0c3f5f90319578a9a5ae8af8d224ed94583
mickaeltartar <mickael.tartar@hotmail.com>
2023-04-26 07:06:12 -0700

    betty style

--- builtins.c
+++ builtins.c
@@ -1,8 +1,9 @@
 #include "main.h"
-
-struct builtin builtins[] = 
-{
-    {"help", shell_help},
-    {"exit", shell_exit},
-    {"cd", shell_cd},
-};
+/**
+ * struct builtin - struct for command shell
+ */
+struct builtin builtins[] = {
+	{"help", shell_help},
+	{"exit", shell_exit},
+	{"cd", shell_cd}
+}
