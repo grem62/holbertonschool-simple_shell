6b12dbfee36219b694f2aa7a0d5b962624aebb57
mickaeltartar <mickael.tartar@hotmail.com>
2023-04-26 07:33:08 -0700

    betty style update

--- shell_command.c
+++ shell_command.c
@@ -3,53 +3,67 @@
 /*====Implémentation des fonctions utilitaires====*/
 
 /**
-*sshell_num_builtins - Renvoie le nombre de commandes enregistrées
-*Return: la taille
+* sshell_num_builtins - Renvoie le nombre de commandes enregistrées
+* Return: la taille
 */
-int sshell_num_builtins()
+int sshell_num_builtins(void)
 {
-    int i;
-    i = 0;
-    while (builtins[i].name != NULL)
-    {
-        i++;
-    }
-    return i;
+	int i;
+
+	i = 0;
+
+	while (builtins[i].name != NULL)
+	{
+		i++;
+	}
+	return (i);
 }
 
 /*====Implementation des builtins====*/
 
+/**
+ * shell_help - affiche le guide de commande "aide"
+ * @args: array of char
+ */
 void shell_help(char **args __attribute__((unused)))
 {
 	int i;
 
-    printf("Simple Shell développé par TARTAR Mickael - LENNE Sebastien - GREMONT Matheo- Holberton School LILLE #C20\n");
-    printf("Les commandes sont disponibles dans le fichier man:\n");
+	printf("Simple Shell auteur");
+	printf("TARTAR Mickael - LENNE Sebastien - GREMONT Matheo");
+	printf("Holberton School LILLE #C20\n");
+	printf("Les commandes sont disponibles dans le fichier man:\n");
 
-    for (i = 0; i < sshell_num_builtins(); i++)
-    {
-        printf("  %s\n", builtins[i].name);
-    }
+	for (i = 0; i < sshell_num_builtins(); i++)
+	{
+		printf("  %s\n", builtins[i].name);
+	}
 
-    printf("Tapez man suivi du nom d'une commande pour plus d'informations.\n");
+	printf("Tapez man suivi du nom d'une commande pour plus d'informations.\n");
 }
-
+/**
+ * shell_exit - exit the shell
+ * @args: array of char
+ */
 void shell_exit(char **args __attribute__((unused)))
 {
-    exit(0);
+	exit(0);
 }
-
+/**
+ * shell_cd - cd is the command for list files
+ * @args: array of char
+ */
 void shell_cd(char **args __attribute__((unused)))
 {
-    if (args[1] == NULL)
-    {
-        fprintf(stderr, "Erreur : argument attendu pour \"cd\".\n");
-    }
-    else
-    {
-        if (chdir(args[1]) != 0)
-        {
-            perror("Erreur");
-        }
-    }
+	if (args[1] == NULL)
+	{
+		fprintf(stderr, "Erreur : argument attendu pour \"cd\".\n");
+	}
+	else
+	{
+		if (chdir(args[1]) != 0)
+		{
+			perror("Erreur");
+		}
+	}
 }
