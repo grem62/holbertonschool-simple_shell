695633c8b4abfcebbc747fd66ad51a16f8541899
lenne sebastien <sebastienlenne@gmail.com>
2023-04-28 20:58:43 +0200

    file  update 7  main.c

--- main.c
+++ main.c
@@ -10,16 +10,18 @@ int main(void)
 	char *line;
 	char **tokens = NULL;
 	
-	write(STDOUT_FILENO, "$", 1);
+	write(STDOUT_FILENO, "bonjour,", 8);
 
 	while (true) /*Boucle while infinie*/
 	{
 		line = sshell_read_line();/*Lire la ligne d'entrée de l'utilisateur*/
 
 		if (line == NULL)
+		{
 			continue;
-
+		}
 		tokens = sshell_split_line(line);/*spliter ligne d'entrée*/
+
 		if (tokens == NULL)
 		{
 			continue;
